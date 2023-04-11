e26ad2aa044e1efe6d1ad5d71d168c930b29cd2a
Alex Kim <ajk8710@nyu.edu>
2023-04-11 12:42:54 -0400

    Day 3 materials

--- MERN 01/overloading.js
+++ MERN 01/overloading.js
@@ -16,13 +16,13 @@ function Area(a, b, c) {
     return "Three Areas"
 }
 
-console.log( Area(1,2,3)) // Three Areas?
+console.log(Area(1,2,3)) // Three Areas?
 
 function Area(b, c) {
     return "Two Areas"
 }
 
-console.log( Area(2,3)) // Two Areas?
+console.log(Area(2,3)) // Two Areas?
 
 // In Javascript, there is no overloading. There is overwriting.
 // Whatever the last function definition overwrites all other functions with same name.
