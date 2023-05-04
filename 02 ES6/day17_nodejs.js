b2ba8699a5e51ecd0d423ce6f250d698f6386aa6
Alex Kim <ajk8710@nyu.edu>
2023-05-04 11:28:36 -0400

    Day 17 Material: NodeJS & REST

---
+++ 02 ES6/day17_nodejs.js
@@ -0,0 +1,49 @@
+// NODE - is a framework to run js on server, also is able to provide everything that 
+// we need to build an entertprse level application
+// I/O - operations
+// db connections
+// file manipulations
+// creating web servers
+// creating http modules
+// etc...
+
+
+// NodeJs - JRE -> whats the difference?
+
+// real time time systems - node js
+// 100s of connections linkdin - can share information including text, images, videos, etc
+// node(live, light weight servers) base systems - points on various servers to store and serve the information
+// majorly it uses flat file structure to save and fetch data
+// event loop based high performant single threaded framework
+
+// limitations -
+// all the task where calculation is involved
+
+
+// Service Oriented Architecture
+// Backend is termed as API or services and responds with data (json) over RESTFul/https channels
+// Fronted is single page, contains logic to generate UI on the fly with the help of server data
+
+// REST => Representational State Transfer Protocol
+// API => Application Programming Interface
+
+// REST Specifications =>
+// Unique End Point = https://google.map.us/state/locations
+// http/s => hyper text type protocol and is stateless
+// Representational information, html5 => hyper text markup language
+
+// For example, if I go to one page of Amazon, then move onto the next - it loses previous state
+// Everything is rendered again when you click the back.
+// Each page is a representaion / a view
+
+// One api should do one job well
+// User - API - should be able to do CRUD operations
+// Repective HTTP methods as below:
+
+// Create - POST
+// Update - PUT/PATCH
+// Delete - Delete
+// GetUser - Get
+// GetUserByID - Get/ID
+
+// Request <==> Response (handshake)
